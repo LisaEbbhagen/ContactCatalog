3312bd99dac639ffe297725833234c3b0fc74e84
LisaEbbhagen <lisa.ebbhagen@gmail.com>
2025-10-24 10:34:38 +0200

    Implementerat fungerande test o mockning.

--- ContactValidator.cs
+++ ContactValidator.cs
@@ -17,32 +17,30 @@ namespace ContactCatalog1
 
         public void Validate(Contact contact)
         {
-            if (!IsValidEmail(contact.Email))
+            if (!EmailValidator.IsValidEmail(contact.Email))
                 throw new InvalidEmailException(contact.Email);
 
             if (_repo.GetAll().Any(c => c.Email.Equals(contact.Email, StringComparison.OrdinalIgnoreCase)))
                 throw new DuplicateEmailException(contact.Email);
         }
 
-        private bool IsValidEmail(string email)
-        {
-            try
-            {
-                var addr = new System.Net.Mail.MailAddress(email);
-                return addr.Address == email;
-            }
-            catch 
-            { 
-                return false; 
-            }
-        }
+        //public bool IsValidEmail(string email)
+        //{
+        //    try
+        //    {
+        //        var addr = new System.Net.Mail.MailAddress(email);
+        //        return addr.Address == email;
+        //    }
+        //    catch 
+        //    { 
+        //        return false; 
+        //    }
+    }
         //_byId.Add(c.Id, c);
         //if (string.IsNullOrWhiteSpace(contact.Email) || !contact.Email.Contains("@"))
         //    throw new InvalidEmailException();
 
         //if (_repo.GetAll().Any(c => c.Email.Equals(contact.Email, StringComparison.OrdinalIgnoreCase)))
-        //    throw new DuplicateEmailException();
-
-    }
+        //    throw new DuplicateEmailException();   
 }
 
